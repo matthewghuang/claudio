872108a293bcd887521c869019cd3df3b891b85e
matthewghuang <matthewhhuang@gmail.com>
2021-01-28 17:19:48 -0800

    basic home screen

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,12 @@
-<script lang="ts">
+<script>
 	import "./Tailwind.svelte"
+	import HomeScreen from "./screens/HomeScreen.svelte"
 	import RideScreen from "./screens/RideScreen.svelte"
 	import page from "page"
 
 	let screen_component
 
+	page("/", () => (screen_component = HomeScreen))
 	page("/ride", () => (screen_component = RideScreen))
 	page()
 </script>
---
+++ src/screens/HomeScreen.svelte
@@ -0,0 +1,12 @@
+<div class="flex w-screen h-screen bg-gray-100 justify-center items-center">
+	<div class="flex flex-wrap space-y-3 justify-center items-center w-3/5">
+		<a class="button" href="/ride">Start Ride</a>
+		<a class="button">Connect Sensors</a>
+	</div>
+</div>
+
+<style>
+	.button {
+		@apply bg-white p-3 rounded-lg w-full text-center;
+	}
+</style>
